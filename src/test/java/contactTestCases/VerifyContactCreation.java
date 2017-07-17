90b9ad974ffc53e8ce3983066ac82ea043f7c72f
sawood777 <praveen.kumar@keastone.com>
2017-07-17 17:10:10 +0530

    contacttestccase

---
+++ src/test/java/contactTestCases/VerifyContactCreation.java
@@ -0,0 +1,13 @@
+package contactTestCases;
+
+import Pages.DashboardPage;
+import Pages.MailPage;
+import emailTestCases.BaseTest;
+
+public class VerifyContactCreation extends BaseTest{
+	
+	DashboardPage d= new DashboardPage(driver);
+	MailPage m= new MailPage(driver);
+	
+
+}
