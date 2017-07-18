0799b025ace6e9009040d0f0cfd362e3eef1eb56
sawood777 <praveen.kumar@keastone.com>
2017-07-18 13:07:47 +0530

    quick contact

--- src/test/java/contactTestCases/VerifyContactCreation.java
+++ src/test/java/contactTestCases/VerifyContactCreation.java
@@ -11,13 +11,47 @@ import Pages.DashboardPage;
 import emailTestCases.BaseTest;
 
 public class VerifyContactCreation extends BaseTest{
+	
+	
 	@Test(priority=1)
+	public void testContactQuickCreate()
+	{
+		boolean flag=false;
+		DashboardPage d= new DashboardPage(driver);
+		ContactPage c= new ContactPage(driver);
+		c.clickQuickContactIcon();
+		c.enterQuickContactName("1");
+		c.enterQuickContactEmail("1@1.com");
+		c.clickCreateButton();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		d.clickContactCard();
+		List<WebElement> sub1=c.getContactList();
+		for(int i=0;i<sub1.size();i++)
+		{
+			
+			if(sub1.get(i).getText().equals("1"))
+			{
+				flag=true;
+				break;
+			}
+		}
+		Assert.assertEquals((flag==true), true, "create contact is unsucessfully");
+		
+	
+	
+	}
+	
+	@Test(priority=2)
 	public void testCreateContact()
 	{
 		boolean flag=false;
 	DashboardPage d= new DashboardPage(driver);
 	ContactPage c= new ContactPage(driver);
-	d.clickContactCard();
 	c.clickCreateIcon();
 	c.enterDisplayName("praveen");
 	c.enterEmailAddress("1@1.com");
@@ -38,15 +72,7 @@ public class VerifyContactCreation extends BaseTest{
 	Assert.assertEquals((flag==true), true, "create contact is unsucessfully");
 	
 }
-	@Test(priority=2)
-	public void testSearchContact()
-	{
-		ContactPage c= new ContactPage(driver);
-		c.clickSeachIcon();
-		
 	
-	
-	}
 	
 	}
 	
